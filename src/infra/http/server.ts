069b07bc7567338f1175fed62cf44ba285aab8d2
salvatoreDeploy <liderhenrique@gmail.com>
2023-03-01 18:38:11 -0300

    atualization

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -4,7 +4,12 @@ import { appRoutes } from "./routes";
 
 const app = fastify();
 
-app.register(cors);
+app.register(cors, {
+  origin: ["*"],
+  methods: "GET,POST",
+});
 app.register(appRoutes);
 
-app.listen({ port: 3333 }).then(() => console.log("HTTP Server Running!"));
+app
+  .listen({ port: 3333, host: "0.0.0.0" })
+  .then(() => console.log("HTTP Server Running!"));
